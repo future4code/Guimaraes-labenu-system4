15298e0e17eafd38f1626f6c3dcbfca1b99cbea8
Ana Carolina Kazue Inada <kazu@MacBook-Air-de-Ana.local>
2022-07-18 21:38:06 -0300

    template

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// import { Request, Response } from "express";
+// import { app } from "./app";
+// import { Character } from "./data/Character";
+// import { CharacterDataBase } from "./data/CharacterDataBase";
+// import { connection } from "./data/connection";
+
+import { app } from "./app";
+
+app;
